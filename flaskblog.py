64c19e511c7aaba2a2ce57f3707916a36bf9f169
Rakesh Kumar <rkakumar85@gmail.com>
2018-09-02 16:32:21 -0500

    Made changes to flaskblog.py as per series 1 of CSchafer Video. Also     added .gitignore file to ignore __pychache__ and .idea file/folders

--- flaskblog.py
+++ flaskblog.py
@@ -3,5 +3,11 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def hello():
-    return "Hello World!"
+    return "<h1>Home Page!</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
